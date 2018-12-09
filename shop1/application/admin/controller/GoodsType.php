6777b215d3183e61b5dbf477c4a701fb7c59d80c
吴仙女 <1510939699@qq.com>
2018-12-09 16:54:25 +0800

    add shop1

---
+++ shop1/application/admin/controller/GoodsType.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/9
+ * Time: 15:39
+ */
+
+namespace app\admin\controller;
+
+
+class GoodsType
+{
+    public function index()
+    {
+        return view();
+    }
+}
