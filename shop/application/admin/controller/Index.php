a2b6062d176f35ede7b2f87f6e4dd99b4e9fea04
吴仙女 <1510939699@qq.com>
2018-12-09 17:12:30 +0800

    add shop

---
+++ shop/application/admin/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'admin';
+    }
+
+    public function login()
+    {
+        return view();
+    }
+}
