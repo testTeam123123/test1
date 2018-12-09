d6067180aa79d86e1981f52afbcc3173332c5f94
wangqian111 <414322094@qq.com>
2018-12-09 16:07:48 +0800

    add shop

---
+++ shop/application/admin/behavior/LoginTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Qian
+ * Date: 2018/12/9
+ * Time: 15:52
+ */
+
+namespace admin\behavior;
+
+
+class LoginTest
+{
+    public function run()
+    {
+
+    }
+}
